94da1b30c44bc6e3034db5dd507a2599202804d9
Rafael Santana de Almeida <rafaelsantana.devweb@gmail.com>
2020-09-07 11:26:48 -0300

    Project Be The Hero - Rocketseat

---
+++ backend/src/controllers/OngController.js
@@ -0,0 +1,22 @@
+const generateUniqueId = require('../utils/generateUniqueId');
+const connetion = require('../database/connection');
+
+module.exports = {
+  async index(request, response) {
+    const ongs = await connetion('ongs').select('*');
+
+    return response.json(ongs);
+  },
+
+  async create(request, response) {
+    const { name, email, whatsapp, city, uf }  = request.body;
+
+    const id = generateUniqueId();
+
+    await connetion('ongs').insert({
+      id, name, email, whatsapp, city, uf,
+    });
+
+    return response.json({ id });
+  }
+}
